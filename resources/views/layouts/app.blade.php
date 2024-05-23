d4de5b317248e1eb4cc4a53ff326772f9695ab48
Martins Ayoola <omartins365@gmail.com>
2024-05-23 14:09:18 +0100

    adjust background

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,6 @@
 @extends('layouts.html')
 @section('body')
-<div id="app" class="" style="
-    min-height: calc(100vh - 68.05px - 65.33px);
-    background-image: url('{{asset("images/asset/background.png")}}')
-">
+<div id="app" >
     <main class="mb-5 pb-5 mt-5">
 
         @yield('content')
--- resources/views/layouts/html.blade.php
+++ resources/views/layouts/html.blade.php
@@ -6,7 +6,10 @@
 <!-- <link href="{{ asset('css/sidebar.css') }}" rel="stylesheet"> -->
 </head>
 
-<body>
+<body class="" style="
+    min-height: calc(100vh - 68.05px - 65.33px);
+    background-image: url('{{asset("images/asset/background.png")}}')
+">
      @include('layouts.message_box')
     @yield('body') <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
